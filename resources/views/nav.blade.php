f84cf2b5d2c26daed2bd71bcc26377da6a836a40
Armndo <armndo.g@gmail.com>
2019-01-15 02:16:37 -0600

    agregar rutas al nav

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,41 +1,39 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Planea tu bien</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
-          Sucursales
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Alta</a>
-          <a class="dropdown-item" href="#">Búsqueda</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Precargas</a>
-        </div>
-	  </li>
-	  <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
-          Clientes
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Alta</a>
-          <a class="dropdown-item" href="#">Búsqueda</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Precargas</a>
-        </div>
-	  </li>
-	  <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
-          Agentes	
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="{{route('empleados.create')}}">Alta</a>
-          <a class="dropdown-item" href="{{route('empleados.index')}}">Búsqueda</a>
-        </div>
-      </li>
-    </ul>
-  </div>
+	<a class="navbar-brand" href="#">Planea tu bien</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+					Sucursales
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="{{route('sucursals.create')}}">Alta</a>
+					<a class="dropdown-item" href="{{route('sucursals.index')}}">Búsqueda</a>
+				</div>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+					Clientes
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="#">Alta</a>
+					<a class="dropdown-item" href="#">Búsqueda</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Precargas</a>
+				</div>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+					Agentes	
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="{{route('empleados.create')}}">Alta</a>
+					<a class="dropdown-item" href="{{route('empleados.index')}}">Búsqueda</a>
+				</div>
+			</li>
+		</ul>
+	</div>
 </nav>
