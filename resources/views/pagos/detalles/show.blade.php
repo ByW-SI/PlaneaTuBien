67275e78f958326271db01971d2f51539990fa9e
Cristian <cristianguzmansuarez@gmail.com>
2019-08-19 11:09:19 -0500

    Se aniadio monto pagado

--- resources/views/pagos/detalles/show.blade.php
+++ resources/views/pagos/detalles/show.blade.php
@@ -6,11 +6,20 @@
             <div class="row">
                 <div class="col-12 col-md-4 form-group">
                     <label><b>SPEI</b></label><br>
-                    {{$pago->spei}}
+                    @if ($pago)
+                        {{$pago->spei}}
+                    @else
+                        N/A
+                    @endif
+                    
                 </div>
                 <div class="col-12 col-md-4 form-group">
                     <label for="cliente"><strong>CLIENTE</strong></label><br>
-                    {{$presolicitud->nombre." ".$presolicitud->paterno." ".$presolicitud->materno}}
+                    @if ($presolicitud)
+                        {{$presolicitud->nombre." ".$presolicitud->paterno." ".$presolicitud->materno}}
+                    @else
+                        N/A
+                    @endif
                 </div>
                 <div class="col-12 col-md-4">
                     <label for="fecha"><strong>FECHA</strong></label><br>
@@ -18,7 +27,11 @@
                 </div>
                 <div class="col-12 col-md-4">
                     <label for="contrato"><strong># CONTRATO</strong></label><br>
-                    {{$deposito_efectivo->contrato()->first()->numero_contrato}}
+                    @if ($deposito_efectivo->contrato()->first())
+                        {{$deposito_efectivo->contrato()->first()->numero_contrato}}
+                    @else
+                        N/A
+                    @endif
                 </div>
                 <div class="col-12 col-md-4">
                     <label for="referencia"><strong>REFERENCIA</strong></label><br>
@@ -29,7 +42,7 @@
             <div class="row">
                 <div class="col-12 col-md-3">
                     <label for="monto_pagado"><strong>MONTO PAGADO</strong></label>
-                    <input type="text" class="form-control" name="monto_pagado" id="monto_pagado" value="{{$pago->monto}}">
+                    <input type="text" class="form-control" name="monto_pagado" id="monto_pagado" value="@if($pago){{$pago->monto}}@else{{'0'}}@endif">
                 </div>
                 <div class="col-12 col-md-3">
                     <label for="monto_a_pagar"><strong>MONTO A PAGAR</strong></label>
