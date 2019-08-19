80e13572b1d915675c5ca1eab5018a82b3983ee1
Cristian <cristianguzmansuarez@gmail.com>
2019-08-19 10:57:50 -0500

    Se aniadio mostrar imagen en detalles de cada una de las referencias

--- app/Http/Controllers/EstadoCuentaController.php
+++ app/Http/Controllers/EstadoCuentaController.php
@@ -3,22 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\DepositoEfectivo;
+use App\Pagos;
 use Illuminate\Http\Request;
 
 class EstadoCuentaController extends Controller
 {
     public function detalle(Request $request)
     {
-        
+
         $deposito_efectivo = DepositoEfectivo::where('id', $request->input('deposito_id'))->first();
-        $presolicitud=null;
-        
-        if ($deposito_efectivo->contrato()->first()){
-            if ($deposito_efectivo->contrato()->first()->presolicitud()->first()){
+        $presolicitud = $this->getPresolicitudByDepositoEfectivo($deposito_efectivo);
+        $pago = Pagos::where('referencia', $deposito_efectivo->concepto)->first();
+
+        return view('pagos.detalles.show', compact('deposito_efectivo', 'presolicitud', 'pago'));
+    }
+
+    public function getPresolicitudByDepositoEfectivo($deposito_efectivo)
+    {
+        $presolicitud = null;
+        if ($deposito_efectivo->contrato()->first()) {
+            if ($deposito_efectivo->contrato()->first()->presolicitud()->first()) {
                 $presolicitud = $deposito_efectivo->contrato()->first()->presolicitud()->first();
             }
         }
-
-        return view('pagos.detalles.show', compact('deposito_efectivo','presolicitud'));
+        return $presolicitud;
     }
 }
--- resources/views/pagos/detalles/show.blade.php
+++ resources/views/pagos/detalles/show.blade.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-12 col-md-4 form-group">
                     <label><b>SPEI</b></label><br>
-                    N/A
+                    {{$pago->spei}}
                 </div>
                 <div class="col-12 col-md-4 form-group">
                     <label for="cliente"><strong>CLIENTE</strong></label><br>
@@ -18,7 +18,7 @@
                 </div>
                 <div class="col-12 col-md-4">
                     <label for="contrato"><strong># CONTRATO</strong></label><br>
-                    N/A
+                    {{$deposito_efectivo->contrato()->first()->numero_contrato}}
                 </div>
                 <div class="col-12 col-md-4">
                     <label for="referencia"><strong>REFERENCIA</strong></label><br>
@@ -29,7 +29,7 @@
             <div class="row">
                 <div class="col-12 col-md-3">
                     <label for="monto_pagado"><strong>MONTO PAGADO</strong></label>
-                    <input type="text" class="form-control" name="monto_pagado" id="monto_pagado">
+                    <input type="text" class="form-control" name="monto_pagado" id="monto_pagado" value="{{$pago->monto}}">
                 </div>
                 <div class="col-12 col-md-3">
                     <label for="monto_a_pagar"><strong>MONTO A PAGAR</strong></label>
@@ -43,10 +43,16 @@
             </div>
             <br>
             <div class="row">
-                <div class="col-12 col-sm-4 form-group">
-                    <input type="file" class="custom-file-input" id="imagen">
-                    <label class="custom-file-label" for="imagen">Subir imagen</label>
-                </div>
+                @if( isset($pago->file_comprobante) && !empty($pago->file_comprobante) )
+                    <div class="col-12 col-sm-4 form-group">
+                        <img src="img/comprobantes/{{$pago->file_comprobante}}" class="img-fluid">
+                    </div>
+                @else
+                    <div class="col-12 col-sm-4 form-group">
+                        <input type="file" class="custom-file-input" id="imagen">
+                        <label class="custom-file-label" for="imagen">Subir imagen</label>
+                    </div>
+                @endif
             </div>
         </form>
 </div>
