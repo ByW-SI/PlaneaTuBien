bbd0bd4fb9cd27e8b627df9245c7a8f671ddd4d3
Cristian <cristianguzmansuarez@gmail.com>
2019-08-23 12:03:36 -0500

    Se modifico schedul job para que se ejecute cada mes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('update:mensualidades')
-                 ->everyMinute();
+                 ->monthlyOn(7,'00:00');
     }
 
     /**
