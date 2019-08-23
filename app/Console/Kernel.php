a544075546dad321d8505186078a95f3d92e3bf1
Cristian <cristianguzmansuarez@gmail.com>
2019-08-23 12:05:41 -0500

    Cada 8 se ejecuta la tarea

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('update:mensualidades')
-                 ->monthlyOn(7,'00:00');
+                 ->monthlyOn(8,'00:00');
     }
 
     /**
