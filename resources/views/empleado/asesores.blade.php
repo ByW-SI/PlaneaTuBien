f1b4c7d84b269016f67700cad54f5b42dae3af1b
Cristian <cristianguzmansuarez@gmail.com>
2019-09-04 10:10:48 -0500

    Cambios

--- resources/views/empleado/asesores.blade.php
+++
@@ -1,4 +0,0 @@
-<option value="">Seleccionar</option>
-@foreach($asesores as $asesor)
-	<option value="{{ $asesor->id }}">{{ $asesor->nombre }} {{ $asesor->paterno }}</option>
-@endforeach
--- resources/views/empleado/listaempleado.blade.php
+++
@@ -1,4 +0,0 @@
-<option value="">Seleccionar</option>
-@foreach($empleados as $empleado)
-	<option value="{{ $empleado->id }}">{{ $empleado->nombre }}</option>
-@endforeach
