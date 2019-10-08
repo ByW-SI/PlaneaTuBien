bd7ac710b138daff3319cf8bb61212b780af6091
Cristian <cristianguzmansuarez@gmail.com>
2019-10-08 09:31:54 -0500

    Cambios

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -68,30 +68,30 @@
 										</a>
 										<button type="button" class="btn btn-danger eliminar" data-toggle="modal" data-target="#exampleModal{{$empleado->id}}" id-empleado={{$empleado->id}}>Eliminar</button>
 										<div class="modal fade" id="exampleModal{{$empleado->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-												<div class="modal-dialog" role="document">
-												  <div class="modal-content">
-													<div class="modal-header">
-													  <h5 class="modal-title" id="exampleModalLabel">Eliminar usuario</h5>
-													  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-														<span aria-hidden="true">&times;</span>
-													  </button>
-													</div>
-													<div class="modal-body">
-															<form action="{{url('empleados', [$empleado->id])}}" method="POST" class="mt-1">
-																@csrf
-																{{method_field('DELETE')}}
-																<div class="form-group">
-																	<label for="motivo" class="col-form-label">Motivo de la baja:</label>
-																	<input type="text" class="form-control" name="motivo">
-																</div>
-																<input type="hidden" name="empleado_id" value="{{$empleado->id}}">
-																<button type="submit" class="btn btn-danger">
-																	Eliminar
-																</button>
-															</form>
-												  </div>
+										<div class="modal-dialog" role="document">
+											<div class="modal-content">
+												<div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLabel">Eliminar usuario</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+													</button>
 												</div>
-											  </div>
+												<div class="modal-body">
+													<form action="{{url('empleados', [$empleado->id])}}" method="POST" class="mt-1">
+														@csrf
+														{{method_field('DELETE')}}
+														<div class="form-group">
+															<label for="motivo" class="col-form-label">Motivo de la baja:</label>
+															<input type="text" class="form-control" name="motivo">
+														</div>
+														<input type="hidden" name="empleado_id" value="{{$empleado->id}}">
+														<button type="submit" class="btn btn-danger">
+															Eliminar
+														</button>
+													</form>
+												</div>
+											</div>
+										</div>
 									</td>
 								</tr>
 								@endforeach
