f94d96aa655acfd7f5941743f69cd1e844ebb82c
Cristian <cristianguzmansuarez@gmail.com>
2020-01-17 21:06:23 -0600

    En el seguimiento de llamadas ahora los statuts realizan la accion pertinente dependiendo del status de la llamada

--- app/Http/Controllers/Api/ApiSeguimientoLlamadaController.php
+++ app/Http/Controllers/Api/ApiSeguimientoLlamadaController.php
@@ -7,32 +7,13 @@ use App\Http\Controllers\Controller;
 use App\Prospecto;
 use App\ResultadoLlamada;
 use App\SeguimientoLlamadas;
+use App\Services\SeguimientoLlamada\StoreSeguimientoLlamadaService;
 
 class ApiSeguimientoLlamadaController extends Controller
 {
     public function store(Request $request)
     {
-        $prospecto = Prospecto::find($request->prospectoId);
-        $resultadoLLamada = ResultadoLlamada::find($request->estatus);
-
-        try {
-            SeguimientoLlamadas::create([
-                'asesor_id' => $prospecto->asesor()->first()->id,
-                'prospecto_id' => $prospecto->id,
-                'resultado_llamada_id' => $resultadoLLamada->id,
-                'fecha_siguiente_contacto' => $request->fechaSeguimiento,
-                'fecha_contacto' => date('Y-m-d'),
-                'comentario' => $request->comentario
-            ]);
-
-            return response()->json([
-                'status' => 'success'
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'status' => 'error',
-                'message' => $e->getMessage(),
-            ]);
-        }
+        $storeSeguimientoLlamadaService = new StoreSeguimientoLlamadaService($request);
+        return response()->json( $storeSeguimientoLlamadaService->getResponse() );
     }
 }
---
+++ app/Services/SeguimientoLlamada/StoreSeguimientoLlamadaService.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Services\SeguimientoLlamada;
+
+use App\CitaCancelada;
+use App\Citas;
+use App\EstatusProspecto;
+use App\Prospecto;
+use App\ResultadoLlamada;
+use App\SeguimientoLlamadas;
+use Illuminate\Http\Request;
+
+class StoreSeguimientoLlamadaService
+{
+
+    protected $request;
+    protected $response;
+
+    protected $cita;
+    protected $citaCancelada;
+
+    protected $prospecto;
+    protected $resultadoLLamada;
+    protected $seguimientoLlamada;
+
+
+
+    public function __construct(Request $request)
+    {
+        $this->setRequest($request);
+        $this->prospecto = Prospecto::find($request->prospectoId);
+        $this->resultadoLLamada = ResultadoLlamada::find($request->estatus);
+
+        try {
+            $this->crearSeguimientoLlamada();
+
+            if ($this->clienteNoHaRespondido()) {
+                $this->crearCita();
+                $this->crearCitaCancelada();
+                $this->actualizarEstatusProspecto('Cita Cancelada');
+            }
+            
+            if ($this->requiereCitaPendiente()) {
+                $this->crearCita();
+                $this->actualizarEstatusProspecto('Pendiente');
+            }
+
+            $this->setResponse([
+                'status' => 'success'
+            ]);
+        } catch (\Exception $e) {
+            $this->setResponse([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    /**
+     * =======
+     * METHODS
+     * =======
+     */
+
+    public function actualizarEstatusProspecto($nombreEstatus)
+    {
+        $this->prospecto->update([
+            'estatus_id' => EstatusProspecto::where('nombre', $nombreEstatus)->first()->id
+        ]);
+    }
+
+    public function crearSeguimientoLlamada()
+    {
+        $this->seguimientoLlamada = SeguimientoLlamadas::create([
+            'asesor_id' => $this->prospecto->asesor()->first()->id,
+            'prospecto_id' => $this->prospecto->id,
+            'resultado_llamada_id' => $this->resultadoLLamada->id,
+            'fecha_siguiente_contacto' => $this->request->fechaSeguimiento,
+            'fecha_contacto' => date('Y-m-d'),
+            'comentario' => $this->request->comentario
+        ]);
+    }
+
+    public function crearCita()
+    {
+        $this->cita = Citas::create([
+            'prospecto_id' => $this->prospecto,
+            'clave_preautorizacion' => '',
+        ]);
+    }
+
+    public function crearCitaCancelada()
+    {
+        $this->citaCancelada = CitaCancelada::create([
+            'cita_id' => $this->cita->id,
+        ]);
+    }
+
+    /**
+     * =======
+     * SETTERS
+     * =======
+     */
+
+    public function setRequest($request)
+    {
+        $this->request = $request;
+    }
+
+    public function setResponse($response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * =======
+     * GETTERS
+     * =======
+     */
+
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * ========
+     * BOOLEANS
+     * ========
+     */
+
+    public function clienteNoHaRespondido()
+    {
+        if ($this->resultadoLLamada->nombre == 'No contesta') {
+            return true;
+        }
+        if ($this->resultadoLLamada->nombre == 'Contestadora') {
+            return true;
+        }
+        if ($this->resultadoLLamada->nombre == 'Deje Recado') {
+            return true;
+        }
+        if ($this->resultadoLLamada->nombre == 'Ocupado') {
+            return true;
+        }
+        return false;
+    }
+
+    public function requiereCitaPendiente()
+    {
+        if ($this->resultadoLLamada->nombre == 'Cita pendiente') {
+            return true;
+        }
+        return false;
+    }
+}
