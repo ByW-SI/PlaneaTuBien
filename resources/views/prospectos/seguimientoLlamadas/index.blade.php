00c7c171b37961a979c09b5e75ef4cc6e86b49b3
Cristian <cristianguzmansuarez@gmail.com>
2020-01-22 13:15:25 -0600

    Se corrigio problema de que en seguimiento de llamadas se duplicaban los datos

--- resources/views/prospectos/seguimientoLlamadas/index.blade.php
+++ resources/views/prospectos/seguimientoLlamadas/index.blade.php
@@ -14,78 +14,80 @@
         {{-- <form action="{{ route('seguimiento.llamadas.store') }}" method="POST"> --}}
         {{-- @csrf --}}
         <div class="card-body">
-            <table class="table table-striped table-hover" id="seguimientotable" style="width:100%">
-                <thead>
-                    <tr class="text-center">
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Celular</th>
-                        <th scope="col">Telefono</th>
-                        <th class="table-danger" style=" width: 150px;">Comentario</th>
-                        <th class="table-primary">Fecha actual</th>
-                        <th class="table-primary" style=" width: 150px;">Estatus</th>
-                        <th class="table-primary">Fecha Seguimiento</th>
-                        <th class="table-warning">Fecha actual</th>
-                        <th class="table-warning">Estatus</th>
-                        <th class="table-warning">Fecha Seguimiento</th>
-                        <th class="table-secondary">Fecha actual</th>
-                        <th class="table-secondary">Estatus</th>
-                        <th class="table-secondary">Fecha Seguimiento</th>
-                        <th class="table-dark">Fecha actual</th>
-                        <th class="table-dark">Estatus</th>
-                        <th class="table-dark">Fecha Seguimiento</th>
-                        <th class="table-warning">Fecha actual</th>
-                        <th class="table-warning">Estatus</th>
-                        <th class="table-warning">Fecha Seguimiento</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($prospectos as $key => $prospecto)
-                    <tr>
-                        <th scope="row">{{ $prospecto->fullName }}</th>
-                        <th scope="row">{{ $prospecto->celular }}</th>
-                        <th scope="row">{{ $prospecto->telefono }}</th>
-                        <td nowrap>
-                            <div class="form-group" style="display: block; width: 150px;">
-                                <textarea class="form-control inputComentario" name="comentario[]" rows="3"
-                                    maxlength="500"
-                                    prospectoId="{{$prospecto->id}}">{{ $prospecto->seguimientoLlamadas->count() > 0 && $prospecto->seguimientoLlamadas->last()->comentario !== null ? $prospecto->seguimientoLlamadas->last()->comentario : ""}}</textarea>
-                                <input type="hidden" name="prospecto_id[]" value="{{ $prospecto->id }}">
-                            </div>
-                        </td>
-                        <td nowrap>
-                            <input type="date" name="fecha_contacto[]" class="form-control" value="{{  date("Y-m-d") }}"
-                                readonly="" prospectoId="{{$prospecto->id}}">
-                        </td>
-                        {{-- INPUT STATUS --}}
-                        <td style="display: inline-block; width: 150px;">
-                            <select name="resultado_llamada_id[]" class="form-control estatus inputEstatus"
-                                prospectoId="{{$prospecto->id}}">
-                                <option value="">Seleccionar</option>
-                                @foreach($estatusLlamada as $codigo)
-                                <option value="{{ $codigo->id }}">{{ $codigo->nombre.' ('.$codigo->codigo.')' }}
-                                </option>
-                                @endforeach
-                            </select>
-                        </td>
-                        {{-- INPUT FECHA SEGUIMIENTO --}}
-                        <td nowrap>
-                            <input type="date" name="fecha_siguiente_contacto" prospectoId="{{$prospecto->id}}"
-                                class="form-control fecha_sig_cont fechaSeguimiento" min="{{  date("Y-m-d") }}">
-                            @include('prospectos.seguimientoLlamadas.modalCrearCita', ['prospecto' => $prospecto])
-                        </td>
-                        <!-- FECHAS ANTERIORES -->
-                        @foreach($seguimientoLlamadas[$key] as $reg)
-                        <td nowrap>
-                            {{ $reg[0] }}
-                        </td>
-                        <td nowrap>{{ $reg[1] }}</td>
-                        <td nowrap>{{ $reg[2] }}</td>
+            <div class="table-responsive">
+                <table class="table table-striped table-hover" id="seguimientotable" style="width:100%">
+                    <thead>
+                        <tr class="text-center">
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Celular</th>
+                            <th scope="col">Telefono</th>
+                            <th class="table-danger" style=" width: 150px;">Comentario</th>
+                            <th class="table-primary">Fecha actual</th>
+                            <th class="table-primary" style=" width: 150px;">Estatus</th>
+                            <th class="table-primary">Fecha Seguimiento</th>
+                            <th class="table-warning">Fecha actual</th>
+                            <th class="table-warning">Estatus</th>
+                            <th class="table-warning">Fecha Seguimiento</th>
+                            <th class="table-secondary">Fecha actual</th>
+                            <th class="table-secondary">Estatus</th>
+                            <th class="table-secondary">Fecha Seguimiento</th>
+                            <th class="table-dark">Fecha actual</th>
+                            <th class="table-dark">Estatus</th>
+                            <th class="table-dark">Fecha Seguimiento</th>
+                            <th class="table-warning">Fecha actual</th>
+                            <th class="table-warning">Estatus</th>
+                            <th class="table-warning">Fecha Seguimiento</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($prospectos as $key => $prospecto)
+                        <tr>
+                            <th scope="row">{{ $prospecto->fullName }}</th>
+                            <th scope="row">{{ $prospecto->celular }}</th>
+                            <th scope="row">{{ $prospecto->telefono }}</th>
+                            <td nowrap>
+                                <div class="form-group" style="display: block; width: 150px;">
+                                    <textarea class="form-control inputComentario" name="comentario[]" rows="3"
+                                        maxlength="500"
+                                        prospectoId="{{$prospecto->id}}">{{ $prospecto->seguimientoLlamadas->count() > 0 && $prospecto->seguimientoLlamadas->last()->comentario !== null ? $prospecto->seguimientoLlamadas->last()->comentario : ""}}</textarea>
+                                    <input type="hidden" name="prospecto_id[]" value="{{ $prospecto->id }}">
+                                </div>
+                            </td>
+                            <td nowrap>
+                                <input type="date" name="fecha_contacto[]" class="form-control"
+                                    value="{{  date("Y-m-d") }}" readonly="" prospectoId="{{$prospecto->id}}">
+                            </td>
+                            {{-- INPUT STATUS --}}
+                            <td style="display: inline-block; width: 150px;">
+                                <select name="resultado_llamada_id[]" class="form-control estatus inputEstatus"
+                                    prospectoId="{{$prospecto->id}}">
+                                    <option value="">Seleccionar</option>
+                                    @foreach($estatusLlamada as $codigo)
+                                    <option value="{{ $codigo->id }}">{{ $codigo->nombre.' ('.$codigo->codigo.')' }}
+                                    </option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            {{-- INPUT FECHA SEGUIMIENTO --}}
+                            <td nowrap>
+                                <input type="date" name="fecha_siguiente_contacto" prospectoId="{{$prospecto->id}}"
+                                    class="form-control fecha_sig_cont fechaSeguimiento" min="{{  date("Y-m-d") }}">
+                                @include('prospectos.seguimientoLlamadas.modalCrearCita', ['prospecto' => $prospecto])
+                            </td>
+                            <!-- FECHAS ANTERIORES -->
+                            @foreach($seguimientoLlamadas[$key] as $reg)
+                            <td nowrap>
+                                {{ $reg[0] }}
+                            </td>
+                            <td nowrap>{{ $reg[1] }}</td>
+                            <td nowrap>{{ $reg[2] }}</td>
+                            @endforeach
+                        </tr>
                         @endforeach
-                    </tr>
-                    @endforeach
 
-                </tbody>
-            </table>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div class="card-footer">
             <div class="text-center">
@@ -95,5 +97,23 @@
         {{-- </form> --}}
     </div>
 </div>
+
+
+<script>
+    $(document).on('focusout', '.fechaSeguimiento', async function(){
+        prospectoId = $(this).attr('prospectoId');
+        response = await actualizarDatosProspecto(prospectoId);
+        // console.log('fecha seguimiento lenfght', ();
+    
+        if(($(this).val()).length){
+            if(requiereCita(prospectoId) ){
+                mostrarModalCrear(prospectoId);
+            }else{
+                location.reload(true);
+            }
+        }
+    });
+
+</script>
 
 @endsection
--- resources/views/prospectos/seguimientoLlamadas/modalCrearCita.blade.php
+++ resources/views/prospectos/seguimientoLlamadas/modalCrearCita.blade.php
@@ -161,56 +161,12 @@
     */
     
     $(document).ready(function() {
-        var table = $('#seguimientotable').DataTable({
-            "columnDefs": [{
-                "orderable": false,
-                "targets": [3,4,5,6]
-            }],
-            "scrollX": true,
-            "language": {
-                "sProcessing": "Procesando...",
-                "sLengthMenu": "Mostrar _MENU_ registros",
-                "sZeroRecords": "No se encontraron resultados",
-                "sEmptyTable": "Ningún dato disponible en esta tabla",
-                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sInfoPostFix": "",
-                "sSearch": "Buscar:",
-                "sUrl": "",
-                "sInfoThousands": ",",
-                "sLoadingRecords": "Cargando...",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "sNext": "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "oAria": {
-                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                }
-            }
-        });
+        var table = $('#seguimientotable').DataTable();
     
         console.log( $('#perteneceAUsuarioAutenticado').val() );
     });
     
-    $(document).on('focusout', '.fechaSeguimiento', async function(){
-        prospectoId = $(this).attr('prospectoId');
-        response = await actualizarDatosProspecto(prospectoId);
-        // console.log('fecha seguimiento lenfght', ();
     
-        if(($(this).val()).length){
-            if(requiereCita(prospectoId) ){
-                mostrarModalCrear(prospectoId);
-            }else{
-                location.reload(true);
-            }
-        }
-    
-        
-    });
     
     $(document).on('change', '.modalCrearCitaInput', function(){
         prospectoId = $(this).attr('prospectoId');
