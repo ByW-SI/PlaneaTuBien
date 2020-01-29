8c159cf5502d9c8f59a7410c2f12855b1df9218e
Cristian <cristianguzmansuarez@gmail.com>
2020-01-28 20:36:21 -0600

    Se corrigio el filtro de citas confirmadas

--- app/Services/Cita/IndexCitaConfirmadaService.php
+++ app/Services/Cita/IndexCitaConfirmadaService.php
@@ -14,6 +14,7 @@ class IndexCitaConfirmadaService
     {
         $this->setCitas();
         $this->filtrarCitasPorFechaDeInicio($request->fechaCitaInicio);
+        $this->filtrarCitasPorFechaDeFin($request->fechaCitaFin);
     }
 
     /**
