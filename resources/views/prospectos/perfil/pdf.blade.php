21a610636eea0c57038d31bc9e61672fa24a6fc4
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-04-26 18:52:47 -0500

    pdf perfil

--- resources/views/prospectos/perfil/pdf.blade.php
+++ resources/views/prospectos/perfil/pdf.blade.php
@@ -416,7 +416,7 @@
           <div class="twelve columns">
             <div class="six columns u-pull-left">
               <div class="ten columns u-pull-left">
-                <label><input type="radio" id="tarjeta_debito_si" {{$perfil->historial_crediticio->tarjeta_debito!=0 ? 'checked="checked"' :""}}>Tarjeta de Débito o Cuenta de Ahorro</label>
+                <label><input type="radio" id="tarjeta_debito_si" {{$perfil->historial_crediticio->numero_tarjeta_debito ? 'checked="checked"' :""}}>Tarjeta de Débito o Cuenta de Ahorro</label>
               </div>
               <div class="two columns u-pull-right">
                 <label style="border-bottom: 0.5px solid #ccc; text-align: center;">{{$perfil->historial_crediticio->numero_tarjeta_debito ? $perfil->historial_crediticio->numero_tarjeta_debito : "0"}}</label>
