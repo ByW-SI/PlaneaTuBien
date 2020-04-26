78977c18655c1287ccfa526d21d380386c0b3639
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-04-26 14:57:39 -0500

    una vez que guarde el perfil, se peuda editar, incluyendo campos que no capture en el pasado

--- resources/views/prospectos/perfil/inmueble_pretendido/info.blade.php
+++ resources/views/prospectos/perfil/inmueble_pretendido/info.blade.php
@@ -39,8 +39,8 @@
 			<div class="col-6">
 				<label>Estado:  </label>
     			<label class="form-control" readonly="">
-    				@if($inmueble['estado'])
-							{{$inmueble['estado']}}
+    				@if($inmueble->estado)
+							{{$inmueble->estado}}
 						@else
 							--
 						@endif
