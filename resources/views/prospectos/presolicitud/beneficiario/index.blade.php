3e2893aaaaf2d9e70909b979937bb9afea026be9
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-04-27 17:42:03 -0500

    referencia Personal de Presolicitud

--- resources/views/prospectos/presolicitud/beneficiario/index.blade.php
+++ resources/views/prospectos/presolicitud/beneficiario/index.blade.php
@@ -6,7 +6,7 @@
 	<div class="card-body">
 		<div class="d-flex justify-content-center">
 			<a 
-			href="{{ route('prospectos.presolicitud.beneficiarios.edit',['prospecto'=>$prospecto,'presolicitud'=>$presolicitud],'active'=>'Beneficiario') }} }}" 
+			href="{{ route('prospectos.presolicitud.beneficiarios.edit',['prospecto'=>$prospecto,'presolicitud'=>$presolicitud]) }} }}" 
 
 			class="btn btn-success">Editar datos personales</a>
 		</div>
