26f232ad7064a7c8435f1ad1cb4ed39090752555
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-04-27 16:48:19 -0500

    referencia Personal de Presolicitud

--- app/Http/Controllers/Prospecto/Cliente/Presolicitud/PresolicitudBeneficiarioController.php
+++ app/Http/Controllers/Prospecto/Cliente/Presolicitud/PresolicitudBeneficiarioController.php
@@ -97,7 +97,7 @@ class PresolicitudBeneficiarioController extends Controller
      * @param  \App\Beneficiario  $beneficiario
      * @return \Illuminate\Http\Response
      */
-    public function edit(Prospecto $prospecto,Presolicitud $presolicitud,Request $request)
+    public function edit(Prospecto $prospecto,Presolicitud $presolicitud)
     {
         //
         
