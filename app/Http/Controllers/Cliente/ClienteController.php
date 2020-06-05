6c4f87ffa294765bf6ad81df3bdf84c50bd264ca
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-06-05 03:21:38 -0500

    Agregamos gestion y agregamos en la Tabla clientes

--- app/Http/Controllers/Cliente/ClienteController.php
+++ app/Http/Controllers/Cliente/ClienteController.php
@@ -60,7 +60,7 @@ class ClienteController extends Controller
         $ajaxPagos=array();
         foreach ($Presolicituds as $Presolicitud) {
             $Referencias=Referencia::where('presolicitud_id',$Presolicitud->id)->get();
-            array_push ($ajaxPagos,[ $Presolicitud->nombre." ".$Presolicitud->paterno." ".$Presolicitud->materno,1,$Contrato->mensualidades->cantidad,$Presolicitud->tel_casa,$Presolicitud->tel_oficina,$Presolicitud->tel_celular,$Referencia[0]->telefono,$Referencia[1]->telefono,$Referencia[2]->telefono]);
+            array_push ($ajaxPagos,[ $Presolicitud->nombre." ".$Presolicitud->paterno." ".$Presolicitud->materno,1,$Contrato->mensualidade()->cantidad,$Presolicitud->tel_casa,$Presolicitud->tel_oficina,$Presolicitud->tel_celular,$Referencia[0]->telefono,$Referencia[1]->telefono,$Referencia[2]->telefono]);
         }
         return json_encode(['data'=> $ajaxPagos]);
 
