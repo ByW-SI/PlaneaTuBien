6cd1c8589ff0e24fa0e0e047717d25b9357c42e6
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-06-08 23:51:28 -0500

    Importe total de contratos

--- app/Http/Controllers/Cliente/ClienteController.php
+++ app/Http/Controllers/Cliente/ClienteController.php
@@ -60,11 +60,11 @@ class ClienteController extends Controller
         $Contratos=Contrato::where('id',$request->input('idD'))->get();
         $Contrato=$Contratos[0];
         $Mensualidad=Mensualidad::where('contrato_id',$Contrato->id)->get();
-        dd($Mensualidad[0]->cantidad);
+        //dd($Mensualidad[0]->cantidad);
         $ajaxPagos=array();
         foreach ($Presolicituds as $Presolicitud) {
             $Referencias=Referencia::where('presolicitud_id',$Presolicitud->id)->get();
-            array_push ($ajaxPagos,[ $Presolicitud->nombre,1,$Mensualidad,$Presolicitud->tel_casa,$Presolicitud->tel_oficina,$Presolicitud->tel_celular,$Referencias[0]->telefono,$Referencias[1]->telefono,$Referencias[2]->telefono]);
+            array_push ($ajaxPagos,[ $Presolicitud->nombre,1,$Mensualidad[0]->cantidad,$Presolicitud->tel_casa,$Presolicitud->tel_oficina,$Presolicitud->tel_celular,$Referencias[0]->telefono,$Referencias[1]->telefono,$Referencias[2]->telefono]);
         }
         return json_encode(['data'=> $ajaxPagos]);
 
