33f3f8011cb09d9d475d5a93988be18d2a2ab79f
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-06-09 00:23:20 -0500

    Clientes table

--- resources/views/presolicitud_cliente/index.blade.php
+++ resources/views/presolicitud_cliente/index.blade.php
@@ -71,6 +71,7 @@
                                 <th>Estatus del cliente</th>
                                 <th>Ultima gestion</th>
                                 <th>Fecha de la ultima gestion</th>
+                                <th></th>
                             </tr>
                         </thead>
 
@@ -280,7 +281,33 @@
                 }
             }
         });
-
+        $('#clientes').DataTable({
+            pageLength : 5,
+            'language':{
+                "sProcessing":     "Procesando...",
+                "sLengthMenu":     "Mostrar _MENU_ registros",
+                "sZeroRecords":    "No se encontraron resultados",
+                "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                "sInfo":           "Productos _START_ al _END_ de un total de _TOTAL_ ",
+                "sInfoEmpty":      "Productos 0 de un total de 0 ",
+                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix":    "",
+                "sSearch":         "Buscar:",
+                "sUrl":            "",
+                "sInfoThousands":  ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst":    "Primero",
+                    "sLast":     "Último",
+                    "sNext":     "Siguiente",
+                    "sPrevious": "Anterior"
+                },
+                "oAria": {
+                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+            }
+        });
 
     });
     $(document).ready(function(){
