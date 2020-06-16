31de02118f01a8aed5bfff9d5cf4caba110ad03f
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-06-15 21:02:10 -0500

    migracion y mejoras con depositos referenciados en tablas de pagos por asignar

--- database/migrations/2020_06_15_191412_create_refdepositopagos_table.php
+++ database/migrations/2020_06_15_191412_create_refdepositopagos_table.php
@@ -15,8 +15,8 @@ class CreateRefdepositopagosTable extends Migration
     {
         Schema::create('refdepositopagos', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('depositos_efectivo_id')->unsigned();
-            $table->foreign('depositos_efectivo_id')->references('id')->on('depositos_efectivos');
+            $table->integer('deposito_efectivo_id')->unsigned();
+            $table->foreign('deposito_efectivo_id')->references('id')->on('depositos_efectivos');
             $table->integer('pago_id')->unsigned();
             $table->foreign('pago_id')->references('id')->on('pagos');
             $table->timestamps();
