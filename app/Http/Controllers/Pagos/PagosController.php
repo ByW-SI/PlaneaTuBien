97ac01fffcfa0b88adc79c384660992c747cdbca
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-06-27 23:47:57 -0500

    Pagos relacionar voucher

--- app/Http/Controllers/Pagos/PagosController.php
+++ app/Http/Controllers/Pagos/PagosController.php
@@ -58,6 +58,6 @@ class PagosController extends Controller
         if ($Pago) {
             $Pago->update(['status_id' => $request->input('status') ]);
         }
-        return redirect()->refresh()->with('flash_success', 'Thank you,!');
+        return redirect()->back()->refresh()->with('flash_success', 'Thank you,!');
     }
 }
