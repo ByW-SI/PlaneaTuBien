4d5c3d8efcb12a8445ba94374fdffae46d9f9626
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-07-07 21:29:49 -0500

    corrida

--- app/Http/Controllers/Cotizacion/CotizacionController.php
+++ app/Http/Controllers/Cotizacion/CotizacionController.php
@@ -53,15 +53,28 @@ class CotizacionController extends Controller
         $Dia_de_inicio=Carbon::parse($Contrato->created_at);
         $Dia_de_inicio=date($Dia_de_inicio->format('Y-M-d'));
 
-        //$Aportacion=[];
-        //$Cuota=[];
-        //$Seguro_vida=[];
-        //$Seguro_danos=[];
-        //$monto_mes=[];
+        if ($Plan->mes_1) {
+            $aportacion1=$Plan->aportacion_1;
+        }else{
+            $aportacion1=0;
+        }
+        
+        if ($Plan->mes_2) {
+            $aportacion2=$Plan->aportacion_2;
+        }else{
+            $aportacion2=0;
+        }
+        if ($Plan->mes_3) {
+            $aportacion3=$Plan->aportacion_3;
+        }else{
+            $aportacion3=0;
+        }
+
         $Aportacion=$Monto_finaciar/$plazo;
         $Cuota_Admin_monto=$Monto_finaciar*($Cuota_Admin/100);
         $Seguro_vida_monto=$Monto_finaciar*($Seguro_vida/100);
         $Seguro_dano_monto=$Monto_finaciar*($Seguro_dano/100);
+        $Total=$Aportacion+$Cuota_Admin_monto+$Seguro_vida_monto+$Seguro_dano_monto;
         $corridaTabla=[];
         for ($i=0; $i < $plazo; $i++) { 
             if (date('m', strtotime($Dia_de_inicio)) == "06" || date('m', strtotime($Dia_de_inicio)) == "12") {
@@ -69,15 +82,33 @@ class CotizacionController extends Controller
                 $Cuota_Admin_monto=round($Cuota_Admin_monto*(($Factor_Actualizacion/100)+1),2);
                 $Seguro_vida_monto=round($Seguro_vida_monto*(($Factor_Actualizacion/100)+1),2);
                 $Seguro_dano_monto=round($Seguro_dano_monto*(($Factor_Actualizacion/100)+1),2);
+                $Total=$Aportacion+$Cuota_Admin_monto+$Seguro_vida_monto+$Seguro_dano_monto;
                 //dd($Factor_Actualizacion);
             }
+            if ($Plan->mes_1==($i+1)) {
+                $Total=$Total+($Monto*($aportacion1/100));
+            }
+            if ($Plan->mes_2==($i+1)) {
+                $Total=$Total+($Monto*($aportacion2/100));
+            }
+            if ($Plan->mes_3==($i+1)) {
+                $Total=$Total+($Monto*($aportacion3/100));
+            }
+            if ( (date('m', strtotime($Dia_de_inicio))&&(!is_null($Plan->$anual)) == "12") {
+                $Total=$Total+($Monto*($Plan->$anual/100));
+            }
+
+
             array_push($corrida,
-                        array('Aportacion' => $Aportacion , 
+                        array(
+                              'mes'=>$i+1,
+                              'Aportacion' => $Aportacion , 
                               'Cuota'=> $Cuota_Admin_monto,
                               'Seguro_vida'=>$Seguro_vida_monto,
-                              'Seguro_danos'=>$Seguro_dano_monto
+                              'Seguro_danos'=>$Seguro_dano_monto,
+                              'Total'=>$Total
                         ));
-            array_push ($corridaTabla,[ $Aportacion,$Cuota_Admin_monto,$Seguro_vida_monto,$Seguro_dano_monto]);
+            array_push ($corridaTabla,[ $i+1,$Aportacion,$Cuota_Admin_monto,$Seguro_vida_monto,$Seguro_dano_monto,$Total]);
         }
 
         
