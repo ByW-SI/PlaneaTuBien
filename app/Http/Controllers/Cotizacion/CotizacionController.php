23625516c2e67bcfa032e4f4b76778eff3e66d4b
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-07-08 14:42:34 -0500

    corrida mensualidad apex

--- app/Http/Controllers/Cotizacion/CotizacionController.php
+++ app/Http/Controllers/Cotizacion/CotizacionController.php
@@ -152,7 +152,7 @@ class CotizacionController extends Controller
                 $Pagoinicial+=round($corrida[$i]['Total'],2)-round($corrida[$i]['Seguro_danos'],2);
             }
         }
-        //dd([$Pagoinicial,$Pagosegundario,($plazo-$Adjudicacion)]);
+        dd([$Pagoinicial,$Pagosegundario,($plazo-$Adjudicacion)]);
         
         $Pagoinicial=round($Pagoinicial/$Adjudicacion,2);
         $Pagosegundario=round($Pagosegundario/($plazo-$Adjudicacion),2);
