35e6db7545e8466b9a891cfc9185aa06526d82d2
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-08-27 00:17:03 -0500

    Crear pago por mensualidad de cliente

--- app/Http/Controllers/Cotizacion/CotizacionController.php
+++ app/Http/Controllers/Cotizacion/CotizacionController.php
@@ -335,7 +335,7 @@ class CotizacionController extends Controller
         $Mensualidades=[];
         foreach ($Mensualidad as $Mes) {
             if ($Mes->pagado==0) {
-                dd($Mes->Contrato->Presolicitud);
+                dd($Mes->Contrato->Presolicitud->cotizacion);
                 array_push($Mensualidades,
                     [
                           $Mes->descripcion,
