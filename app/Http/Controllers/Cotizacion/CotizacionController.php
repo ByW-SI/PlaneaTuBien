2ad2e474b4d5091d626b81cb01e9ca9afabfb8da
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2020-08-26 21:35:22 -0500

    Actualizar mensualidad con descripcion

--- app/Http/Controllers/Cotizacion/CotizacionController.php
+++ app/Http/Controllers/Cotizacion/CotizacionController.php
@@ -37,41 +37,46 @@ class CotizacionController extends Controller
         $Plan=$Cotizacion->plan;        
 
         $Apex=[];
-
+        $auxApex=0;
         if (!is_null($Plan->aportacion_1)&&!is_null($Plan->mes_1)) {
-            if ($Mensualidades[$Plan->mes_1-1]->pagado==1) {
+            
+            if ($Mensualidades[$auxApex]->pagado==1) {
                 array_push ($Apex,[ "Apex 1",$Plan->aportacion_1,$Plan->mes_1,"Pagado"]);
             }else{
                 array_push ($Apex,[ "Apex 1",$Plan->aportacion_1,$Plan->mes_1,"No pagado"]);
             }
+            $auxApex+=1;
         }else{
             array_push ($Apex,[ "Apex 1","N/A","N/A","N/A"]);
         }
         
         if (!is_null($Plan->aportacion_2)&&!is_null($Plan->mes_2)) {
-            if ($Mensualidades[$Plan->mes_2-1]->pagado==1) {
+            if ($Mensualidades[$auxApex]->pagado==1) {
                 array_push ($Apex,[ "Apex 2",$Plan->aportacion_2,$Plan->mes_2,"Pagado"]);
             }else{
                 array_push ($Apex,[ "Apex 2",$Plan->aportacion_2,$Plan->mes_2,"No pagado"]);
             }
+            $auxApex+=1;
         }else{
             array_push ($Apex,[ "Apex 3","N/A","N/A","N/A"]);
         }
         if (!is_null($Plan->aportacion_3)&&!is_null($Plan->mes_3)) {
-            if ($Mensualidades[$Plan->mes_2-1]->pagado==1) {
+            if ($Mensualidades[$auxApex]->pagado==1) {
                 array_push ($Apex,[ "Apex 3",$Plan->aportacion_3,$Plan->mes_3,"Pagado"]);
             }else{
                 array_push ($Apex,[ "Apex 3",$Plan->aportacion_3,$Plan->mes_3,"No pagado"]);
             }
+            $auxApex+=1;
         }else{
             array_push ($Apex,[ "Apex 3","N/A","N/A","N/A"]);
         }
         if (!is_null($Plan->aportacion_liquidacion)&&!is_null($Plan->mes_liquidacion)) {
-            if ($Mensualidades[$Plan->mes_liquidacion-1]->pagado==1) {
+            if ($Mensualidades[$auxApex]->pagado==1) {
                 array_push ($Apex,[ "Apex Liquidacion",$Plan->aportacion_liquidacion,$Plan->mes_liquidacion,"Pagado"]);
             }else{
                 array_push ($Apex,[ "Apex Liquidacion",$Plan->aportacion_liquidacion,$Plan->mes_liquidacion,"No pagado"]);
             }
+            $auxApex+=1;
         }else{
             array_push ($Apex,[ "Apex Liquidacion","N/A","N/A","N/A"]);
         }
